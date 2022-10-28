2546709b40c8a384652a295bf550861af2c702ef
Emilie Ick-Art <emilie.inack-audibert@le-campus-numerique.fr>
2022-10-28 17:17:21 +0200

    Done 1.1 to 1.6

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 //A toi de jouer ! https://youtu.be/1dLcnxIock0
+
+let id=0
+
+const {createApp} = Vue 
+
+createApp({
+    data(){
+        return{
+            newTodo: '',
+            hideCompleted: false,
+            todos: [
+                { id: id++, text: 'Learn Laravel', done: false},
+                { id: id++, text: 'Learn Vue.js', done: false},
+                { id: id++, text: 'Learn Swagger', done: false},
+            ],
+            errors: []
+        }
+    },
+    computed:{
+        filteredTodos() {
+            return this.hideCompleted
+            ? this.todos.filter((t) => !t.done)
+            : this.todos
+        }
+    },
+    methods: {
+        addTodo() {
+            //this.todos.push({id: id++, text: this.newTodo, done: false})
+            
+            
+            // Vérifier si du texte a bien été saisi
+            this.errors =  []
+
+            if(this.newTodo) {
+                this.todos.push({id: id++, text: this.newTodo, done: false});
+                this.errors = []
+            }
+            
+            if(!this.newTodo){
+                this.errors.push('You need to fill in a todo before submit')
+            }
+
+            this.newTodo = ''
+        },
+        removeTodos() {
+            this.todos = []
+            //this.todos = this.todos.filter((todo) => todo !== todo)
+        }
+    },
+    mounted() {
+        this.todos
+    }
+}).mount('#app')
